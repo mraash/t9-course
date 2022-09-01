90e84b0533c929c4a9fafc55e67229ac11f52166
Marshan Eric <marshansr@gmail.com>
2022-09-01 17:37:26 +0300

    add seeders

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    private $isNamesFilled = false;
+
+    private array $firstNames = [];
+    private array $lastNames = [];
+
+    public function fillNames()
+    {
+        $this->isNamesFilled = true;
+
+        for ($i = 0; $i < 20; $i++) {
+            $this->firstNames[] = $this->faker->unique()->firstName();
+            $this->lastNames[] = $this->faker->unique()->lastName();
+        }
+    }
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string,mixed>
+     */
+    public function definition(): array
+    {
+        if (!$this->isNamesFilled) {
+            $this->fillNames();
+        }
+
+        return [
+            'group_id' => null,
+            'first_name' => $this->faker->randomElement($this->firstNames),
+            'last_name' => $this->faker->randomElement($this->lastNames),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
+use App\Models\Group;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call([
-        ]);
+        $groups = Group::factory(10)->create();
+        $courses = Course::factory(10)->create();
+        $students = Student::factory(200)->create();
+
+        $students->each(function (Student $student) use ($groups, $courses) {
+            $studentCourses = [];
+
+            $courseKeys = array_rand($courses->toArray(), fake()->numberBetween(1, 3));
+            $courseKeys = is_array($courseKeys) ? $courseKeys : [$courseKeys];
+
+            foreach ($courseKeys as $index) {
+                array_push($studentCourses, $courses->get($index));
+            }
+
+            $studentGroup = fake()->randomElement($groups);
+
+            $student->group()->associate($studentGroup)->save();
+            $student->courses()->saveMany($studentCourses);
+        });
     }
 }
