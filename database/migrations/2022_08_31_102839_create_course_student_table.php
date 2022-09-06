76ebcaa47a28566f5c4623fee2eb9804fcafcf34
Marshan Eric <marshansr@gmail.com>
2022-09-07 02:37:58 +0300

    add on delete cascade to course_students table

--- database/migrations/2022_08_31_102839_create_course_student_table.php
+++ database/migrations/2022_08_31_102839_create_course_student_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('course_student', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('student_id')->constrained();
-            $table->foreignId('course_id')->constrained();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('course_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
